0552ba86fae587b0af7f763b2d78944926267564
ryuichiokubo <ryuichi.okubo.1986@gmail.com>
2016-09-09 10:14:49 +0300

    Show info provided to listeners in UI

--- app/src/main/java/com/gmail/a1986/okubo/ryuichi/viewpagerhack/MainActivity.java
+++ app/src/main/java/com/gmail/a1986/okubo/ryuichi/viewpagerhack/MainActivity.java
@@ -1,11 +1,19 @@
 package com.gmail.a1986.okubo.ryuichi.viewpagerhack;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -22,10 +30,16 @@ public class MainActivity extends AppCompatActivity {
      */
     private ViewPager viewPager;
 
+    private TextView debugView;
+    private ScrollView debugContainer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        debugContainer = (ScrollView) findViewById(R.id.debug_container);
+        debugView = (TextView) findViewById(R.id.debug);
 
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
@@ -34,6 +48,57 @@ public class MainActivity extends AppCompatActivity {
         // Set up the ViewPager with the sections adapter.
         viewPager = (ViewPager) findViewById(R.id.container);
         viewPager.setAdapter(sectionsPagerAdapter);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                showLog("onPageScrolled position=" + position + " positionOffset=" + positionOffset + " positionOffsetPixels=" + positionOffsetPixels);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                showLog("onPageSelected position=" + position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                String stateString;
+                switch (state) {
+                    case 0:
+                        stateString = "SCROLL_STATE_IDLE";
+                        break;
+                    case 1:
+                        stateString = "SCROLL_STATE_DRAGGING";
+                        break;
+                    case 2:
+                        stateString = "SCROLL_STATE_SETTLING";
+                        break;
+                    default:
+                        stateString = "ERROR!!";
+                }
+                showLog("onPageScrollStateChanged state=" + stateString);
+            }
+        });
+
+        viewPager.setPageTransformer(false, new ViewPager.PageTransformer() {
+            @Override
+            public void transformPage(View page, float position) {
+                showLog("transformPage page=" + page + " position=" + position);
+            }
+        });
+
+        viewPager.addOnAdapterChangeListener(new ViewPager.OnAdapterChangeListener() {
+            @Override
+            public void onAdapterChanged(@NonNull ViewPager viewPager, @Nullable PagerAdapter oldAdapter, @Nullable PagerAdapter newAdapter) {
+
+            }
+        });
+    }
+
+    private void showLog(String log) {
+        String currentText = debugView.getText().toString();
+        debugView.setText(currentText + '\n' + log);
+        debugContainer.fullScroll(View.FOCUS_DOWN);
     }
 
 }
--- app/src/main/java/com/gmail/a1986/okubo/ryuichi/viewpagerhack/PlaceholderFragment.java
+++ app/src/main/java/com/gmail/a1986/okubo/ryuichi/viewpagerhack/PlaceholderFragment.java
@@ -32,8 +32,10 @@ public class PlaceholderFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
         TextView textView = (TextView) rootView.findViewById(R.id.section_label);
         textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+
         return rootView;
     }
 }
